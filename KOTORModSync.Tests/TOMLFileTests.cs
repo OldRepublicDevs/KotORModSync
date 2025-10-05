5bdf9d0a17ef5af20fac31a0cb277e1d26e43286
Benjamin Auquite <halomastar@gmail.com>
2025-10-05 02:42:10 -0500

    Refactor TOMLFileTests for improved readability and normalization logic
    - Simplified TOML string formatting for better clarity.
    - Updated component initialization to enhance readability.
    - Introduced normalization of instruction GUIDs in comparison logic to ensure consistent test results.

--- KOTORModSync.Tests/TOMLFileTests.cs
+++ KOTORModSync.Tests/TOMLFileTests.cs
@@ -34,50 +34,49 @@ namespace KOTORModSync.Tests
 		private string _filePath = string.Empty;
 
 		// ReSharper disable once ConvertToConstant.Local
-		private readonly string _exampleToml = @"
-            [[thisMod]]
-            name = ""Ultimate Dantooine""
-            guid = ""{B3525945-BDBD-45D8-A324-AAF328A5E13E}""
-            dependencies = [
-                ""{C5418549-6B7E-4A8C-8B8E-4AA1BC63C732}"",
-                ""{D0F371DA-5C69-4A26-8A37-76E3A6A2A50D}""
-            ]
-            installOrder = 3
+		private readonly string _exampleToml = @"[[thisMod]]
+name = ""Ultimate Dantooine""
+guid = ""{B3525945-BDBD-45D8-A324-AAF328A5E13E}""
+dependencies = [
+    ""{C5418549-6B7E-4A8C-8B8E-4AA1BC63C732}"",
+    ""{D0F371DA-5C69-4A26-8A37-76E3A6A2A50D}""
+]
+installOrder = 3
 
-            [[thisMod.instructions]]
-            action = ""extract""
-            source = ""Ultimate Dantooine High Resolution - TPC Version-1103-2-1-1670680013.rar""
-            destination = ""%temp%\\mod_files\\Dantooine HR""
-            overwrite = true
+[[thisMod.instructions]]
+action = ""extract""
+source = ""Ultimate Dantooine High Resolution - TPC Version-1103-2-1-1670680013.rar""
+destination = ""%temp%\\mod_files\\Dantooine HR""
+overwrite = true
 
-            [[thisMod.instructions]]
-            action = ""delete""
-            paths = [
-                ""%temp%\\mod_files\\Dantooine HR\\DAN_wall03.tpc"",
-                ""%temp%\\mod_files\\Dantooine HR\\DAN_NEW1.tpc"",
-                ""%temp%\\mod_files\\Dantooine HR\\DAN_MWFl.tpc""
-            ]
+[[thisMod.instructions]]
+action = ""delete""
+paths = [
+    ""%temp%\\mod_files\\Dantooine HR\\DAN_wall03.tpc"",
+    ""%temp%\\mod_files\\Dantooine HR\\DAN_NEW1.tpc"",
+    ""%temp%\\mod_files\\Dantooine HR\\DAN_MWFl.tpc""
+]
 
-            [[thisMod.instructions]]
-            action = ""move""
-            source = ""%temp%\\mod_files\\Dantooine HR\\""
-            destination = ""%temp%\\Override""
+[[thisMod.instructions]]
+action = ""move""
+source = ""%temp%\\mod_files\\Dantooine HR\\""
+destination = ""%temp%\\Override""
 
-            [[thisMod]]
-            name = ""TSLRCM Tweak Pack""
-            guid = ""{C5418549-6B7E-4A8C-8B8E-4AA1BC63C732}""
-            installOrder = 1
-            dependencies = []
+[[thisMod]]
+name = ""TSLRCM Tweak Pack""
+guid = ""{C5418549-6B7E-4A8C-8B8E-4AA1BC63C732}""
+installOrder = 1
+dependencies = []
 
-            [[thisMod.instructions]]
-            action = ""extract""
-            source = ""URCMTP 1.3.rar""
-            destination = ""%temp%\\mod_files\\TSLRCM Tweak Pack""
-            overwrite = true
+[[thisMod.instructions]]
+action = ""extract""
+source = ""URCMTP 1.3.rar""
+destination = ""%temp%\\mod_files\\TSLRCM Tweak Pack""
+overwrite = true
 
-            [[thisMod.instructions]]
-            action = ""run""
-            path = ""%temp%\\mod_files\\TSLPatcher.exe""";
+[[thisMod.instructions]]
+action = ""run""
+path = ""%temp%\\mod_files\\TSLPatcher.exe""";
 
 		[Test]
 		public void SaveAndLoadTOMLFile_MatchingComponents()
@@ -331,43 +330,43 @@ namespace KOTORModSync.Tests
 			List<List<Component>> rounds =
 			[
 				[
-					new()
+					new Component
 					{
 						Name = "Component 1", Guid = Guid.Parse("{B3525945-BDBD-45D8-A324-AAF328A5E13E}"),
 					},
-					new()
+					new Component
 					{
 						Name = "Component 2", Guid = Guid.Parse("{C5418549-6B7E-4A8C-8B8E-4AA1BC63C732}"),
 					},
 				],
 				[
-					new()
+					new Component
 					{
 						Name = "Component 3", Guid = Guid.Parse("{D0F371DA-5C69-4A26-8A37-76E3A6A2A50D}"),
 					},
-					new()
+					new Component
 					{
 						Name = "Component 4", Guid = Guid.Parse("{E7B27A19-9A81-4A20-B062-7D00F2603D5C}"),
 					},
-					new()
+					new Component
 					{
 						Name = "Component 5", Guid = Guid.Parse("{F1B05F5D-3C06-4B64-8E39-8BEC8D22BB0A}"),
 					},
 				],
 				[
-					new()
+					new Component
 					{
 						Name = "Component 6", Guid = Guid.Parse("{EF04A28E-5031-4A95-A85A-9A1B29A31710}"),
 					},
-					new()
+					new Component
 					{
 						Name = "Component 7", Guid = Guid.Parse("{B0373F49-ED5A-43A1-91E0-5CEB85659282}"),
 					},
-					new()
+					new Component
 					{
 						Name = "Component 8", Guid = Guid.Parse("{BBDB9C8D-DA44-4859-A641-0364D6F34D12}"),
 					},
-					new()
+					new Component
 					{
 						Name = "Component 9", Guid = Guid.Parse("{D6B5C60F-26A7-4595-A0E2-2DE567A376DE}"),
 					},
@@ -441,10 +440,41 @@ namespace KOTORModSync.Tests
 			if ( obj.GetType() != another.GetType() )
 				return;
 
-			string objJson = JsonConvert.SerializeObject(obj);
-			string anotherJson = JsonConvert.SerializeObject(another);
+			// If comparing Components, normalize instruction GUIDs before comparing
+			if ( obj is Component comp1 && another is Component comp2 )
+			{
+				// Create deep copies to avoid modifying the originals
+				string json1 = JsonConvert.SerializeObject(comp1);
+				string json2 = JsonConvert.SerializeObject(comp2);
 
-			Assert.That(objJson, Is.EqualTo(anotherJson));
+				Component copy1 = JsonConvert.DeserializeObject<Component>(json1)!;
+				Component copy2 = JsonConvert.DeserializeObject<Component>(json2)!;
+
+				// Normalize instruction GUIDs - set ALL instruction GUIDs to a fixed value
+				// We can't use Guid.Empty because the Instruction.Guid property auto-generates a new GUID when accessed if empty
+				var fixedGuid = Guid.Parse("00000000-0000-0000-0000-000000000001");
+				foreach ( Instruction instruction in copy1.Instructions )
+				{
+					instruction.Guid = fixedGuid;
+				}
+				foreach ( Instruction instruction in copy2.Instructions )
+				{
+					instruction.Guid = fixedGuid;
+				}
+
+				// Now compare the normalized copies
+				string normalizedJson1 = JsonConvert.SerializeObject(copy1);
+				string normalizedJson2 = JsonConvert.SerializeObject(copy2);
+
+				Assert.That(normalizedJson1, Is.EqualTo(normalizedJson2));
+			}
+			else
+			{
+				string objJson = JsonConvert.SerializeObject(obj);
+				string anotherJson = JsonConvert.SerializeObject(another);
+
+				Assert.That(objJson, Is.EqualTo(anotherJson));
+			}
 		}
 	}
 }
